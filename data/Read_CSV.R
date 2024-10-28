371306994801a5573e63c18f87dcd39126440419
Arushi <adub0005@student.monash.edu>
2024-10-28 20:52:50 +1100

    Styling the shiny app

--- data/Read_CSV.R
+++ data/Read_CSV.R
@@ -3,3 +3,10 @@ tourism <- read_csv("data/tourism-gdp-proportion-of-total-gdp.csv")
 
 usethis::use_data(tourism, overwrite = TRUE)
 
+# Filter data for a single country (e.g., "Australia") and a specific year range
+library(dplyr)
+australia_data <- tourism %>%
+  filter(Entity == "Australia", Year >= 2008, Year <= 2022)
+
+# Display filtered data
+head(australia_data)
