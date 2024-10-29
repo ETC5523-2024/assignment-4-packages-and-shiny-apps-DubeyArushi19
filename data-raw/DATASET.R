5a708d2239fcb37a4744787c85582535594c17c5
Arushi <adub0005@student.monash.edu>
2024-10-29 15:49:54 +1100

    Documentations added for data, functions and created roxygen comments

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,16 @@
-library(readr)
+## code to prepare `DATASET` dataset goes here
+
 tourism <- read_csv("data/tourism-gdp-proportion-of-total-gdp.csv")
 
 usethis::use_data(tourism, overwrite = TRUE)
 
+write.csv(tourism, file = "tourism.csv")
+
 # Filter data for a single country (e.g., "Australia") and a specific year range
-library(dplyr)
 australia_data <- tourism %>%
   filter(Entity == "Australia", Year >= 2008, Year <= 2022)
 
-# Display filtered data
-head(australia_data)
+usethis::use_data(australia_data, overwrite = TRUE)
+write.csv(australia_data, file = "australia_data.csv")
+
+
